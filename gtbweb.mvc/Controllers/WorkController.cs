50ed4d483c5b9ba751414c6d6321b6073f324d5d
DESKTOP-F5VG5HT\OLUKOWI <dolukowi@gmail.com>
2019-03-20 11:29:39 +0100

    Initialization

---
+++ gtbweb.mvc/Controllers/WorkController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using gtbweb.Models;
+
+namespace gtbweb.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Work()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
